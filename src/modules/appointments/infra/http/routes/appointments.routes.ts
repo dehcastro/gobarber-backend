1513fcf6b844064922d6f29b00a97b0dea0faad0
André de Castro <andre_vc@hotmail.com>
2020-10-05 23:23:04 -0300

    :heavy_check_mark: Finalizando back-end do app | Agenda do Prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -3,17 +3,13 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import appointmentsController from '../controllers/AppointmentsController';
+import providerAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//     const appointments = await appointmentsRepository.find();
-
-//     return response.json(appointments);
-// });
-
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
