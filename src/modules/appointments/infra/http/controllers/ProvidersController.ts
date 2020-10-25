58e3c9f01ca39b2e12eecff6531d23f20612921a
André de Castro <andre_vc@hotmail.com>
2020-10-25 16:57:02 -0300

    finished

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
@@ -15,7 +15,7 @@ class ProvidersController {
 
         const providers = await listProviders.execute({ user_id });
 
-        return response.json(providers);
+        return response.json(classToClass(providers));
     }
 }
 
--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
@@ -20,16 +21,21 @@ class ListProvidersService {
     ) {}
 
     public async execute({ user_id }: IRequest): Promise<User[]> {
-        let users = await this.cacheProvider.recover<User[]>(
-            `providers-list:${user_id}`,
-        );
+        // let users = await this.cacheProvider.recover<User[]>(
+        //     `providers-list:${user_id}`,
+        // );
+
+        let users;
 
         if (!users) {
             users = await this.usersRepository.findAllProviders({
                 but_this_user_id: user_id,
             });
 
-            await this.cacheProvider.save(`providers-list:${user_id}`, users);
+            await this.cacheProvider.save(
+                `providers-list:${user_id}`,
+                classToClass(users),
+            );
         }
 
         return users;
